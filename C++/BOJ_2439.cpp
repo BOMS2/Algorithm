2d65190a6a2703c9da1841a75aaf433e6c8fad3e
bomin <kbm97121@swu.ac.kr>
2020-07-22 19:10:01 +0900

    Add BOJ_2439

---
+++ C++/BOJ_2439.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, n=0;
+    cin >> a;
+    n=a;
+    for(int i=0; i<a; i++){
+        for(int j=1; j<n; j++){
+          cout << " ";
+        }
+        for(int j=a; j>a-i; j--){
+            cout << "*";
+        }
+    cout << "*" << "\n";
+    n--;
+    }
+}
