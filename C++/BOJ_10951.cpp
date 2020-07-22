ea62715a533d8027367e3816b531b6f237230fb0
bomin <kbm97121@swu.ac.kr>
2020-07-22 19:22:15 +0900

    Add BOJ_10951

---
+++ C++/BOJ_10951.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+//입력이 있을 경우 true, 입력이 없을 경우 false를 리턴하게 된다.
+int main(void)
+{
+    //테스트케이스 개수가 주어지지 않는다면 EOF 사용해도 된다.
+    int a,b=0;
+    while(cin >> a >> b){
+        cout << a+b << "\n";
+    }
+}
