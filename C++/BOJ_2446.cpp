f174491766948b507bf16aaacd87f0c2b0155cd0
bomin <kbm97121@swu.ac.kr>
2020-07-26 09:59:55 +0900

    Add BOJ_2446

---
+++ C++/BOJ_2446.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,n,m=0;
+    cin >> a;
+    n=a;
+    m=a;
+    //윗부분
+    for(int i=0; i<a; i++){
+        for(int j=a; j>a-i; j--){
+            cout << " ";
+        }
+        for(int j=1; j<n; j++){
+          cout << "*";
+        }
+        for(int j=i+1; j<a; j++){
+            cout << "*";
+        }
+        cout << "*" << "\n";
+        n--;
+    }
+    //아랫부분
+    for(int i=0; i<a; i++){
+        for(int j=1; j<m; j++){
+            if(i != 0) {
+                cout << " ";
+            }
+        }
+        for(int j=a; j>a-i; j--){
+            cout << "*";
+        }
+        for(int k=a; k>m; k--){
+            if(i != 0){
+                cout << "*";
+            }
+        }
+        if(i != 0) {
+            cout << "*" << "\n";
+        }
+        m--;
+    }
+}
